4be5193bfce94946d7be7d6b6909a375518070bb
Letícia França <99284224+LeticiaSFranca@users.noreply.github.com>
2023-08-17 19:04:40 -0300

    Add files via upload

---
+++ Projeto-Console05.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pag57_Ex3_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int empregados = 0, minimo = 0, contagem = 0;
+            string sair = "";
+            double salario = 0, gastos = 0, media = 0, porcentagem = 0;
+
+            while (sair != "0")
+            {
+                Console.Write("Digite o seu salário: ");
+                salario = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (salario < 1212)
+                {
+                    Console.WriteLine("Erro! Digite um valor acima do salário mínimo.");
+                    Console.Write("Digite 0 para sair ou 1 para continuar: ");
+                    sair = Console.ReadLine();
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    gastos = salario + gastos;
+                    Console.Write("Digite 0 para sair ou 1 para continuar: ");
+                    sair = Console.ReadLine();
+                    Console.WriteLine();
+                    empregados++;
+                }
+
+                if (salario == 1212)
+                {
+                    minimo++;
+                }
+
+                if (salario <= 3500)
+                {
+                    contagem++;
+                    porcentagem = (contagem * 100) / empregados;
+                }
+            }
+
+            media = gastos / empregados;
+            Console.WriteLine("");
+            Console.WriteLine("Quantidade de empregados que recebem um salário mínimo: " + minimo);
+            Console.WriteLine("Porcentagem de empregados que ganham entre R$ 1500,00 e R$ 3500,00: " + porcentagem + "%");
+            Console.WriteLine("O gasto total da firma é de: " + "R$ " + Math.Round(media, 2));
+            Console.WriteLine("");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Projeto-Console06.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pag59Ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "", sexo = "", sair = "";
+            int idade, homens = 0, mulheres = 0;
+            double total = 0, media = 0, porcentagem = 0, contagem = 0;
+
+            while (sair != "exit")
+            {
+                Console.Write("Digite seu nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Digite sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.Write("Digite seu sexo (m/f): ");
+                sexo = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("Digite 'exit' para sair ou 'enter' para continuar");
+                sair = Console.ReadLine();
+
+                if (sexo == "M".ToUpper())
+                {
+                    homens++;
+                }
+
+                if (sexo == "F".ToUpper() && idade >= 18)
+                {
+                    mulheres++;
+                }
+
+                if (sexo == "F".ToUpper() && idade >= 20 && idade <= 29)
+                {
+                    contagem++;
+                }
+                total = idade + total;
+            }
+
+            media = total / homens;
+            porcentagem = (contagem * 100) / mulheres;
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Mulheres maiores de 18 anos: " + mulheres);
+            Console.WriteLine("Média de idade dos homens: " + Math.Round(media, 2));
+            Console.WriteLine("Porcentagem de mulheres entre 20 e 29 anos: " + porcentagem + "%");
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
